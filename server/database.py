51c23edd7eb169f6559cabe371113d400b7e44f2
jonnyDurston <jonathan.durston@btinternet.com>
2025-09-06 15:35:04 +0100

    Commit and isolation_level None

--- server/database.py
+++ server/database.py
@@ -9,6 +9,7 @@ async def get_database_connection():
     """
     Gets a database connection
     """
-    async with aiosqlite.connect("vocabulary.db") as conn:
+    async with aiosqlite.connect("vocabulary.db", isolation_level=None) as conn:
         conn.row_factory = aiosqlite.Row
         yield conn
+        conn.commit()
